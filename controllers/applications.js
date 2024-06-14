f7fe4d0cd9c0301d70d7daae98411a5cf75711e2
Steve <zjplove3618739792@gmail.com>
2024-06-13 22:04:00 -0400

    message describing code changes

--- controllers/applications.js
+++ controllers/applications.js
@@ -31,27 +31,37 @@ router.post('/', async (req, res) => {
     }
   });
 
- 
+router.get('/booking-1', async (req, res) => {
+  res.render('applications/booking-1.ejs');
+});
+router.get('/booking-2', async (req, res) => {
+  res.render('applications/booking-2.ejs');
+});
 
-  router.get('/:applicationId', async (req, res) => {
-    try {
-    
-      const currentUser = await User.findById(req.session.user._id);
-      console.log('current user' ,currentUser)
-    
-      const application = currentUser.applications.id(req.params.applicationId);
-      console.log('application ',application)
+router.get('/booking-3', async (req, res) => {
+  res.render('applications/booking-3.ejs');
+});
+router.get('/booking-4', async (req, res) => {
+  res.render('applications/booking-4.ejs');
+});
 
-    
-      res.render(`applications/${req.params.applicationId}.ejs`, {
-        application: application,
-      });
-    } catch (error) {
-      
-      console.log(error);
-      res.redirect('/')
-    }
-  });
+router.get('/:applicationId', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the application by the applicationId supplied from req.params
+    const application = currentUser.applications.id(req.params.applicationId);
+    // Render the show view, passing the application data in the context object
+    res.render('applications/show.ejs', {
+      application: application,
+    });
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
 
 
 router.delete('/:applicationId', async (req, res) => {
